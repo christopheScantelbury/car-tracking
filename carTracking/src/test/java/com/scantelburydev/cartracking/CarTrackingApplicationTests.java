0aa533f9a6d6460a625447b9fd0e39f91c150cb6
Christophe <christopheneves@ext.cvccorp.com.br>
2024-09-21 13:40:30 -0300

    Structure project first activite

---
+++ carTracking/src/test/java/com/scantelburydev/cartracking/CarTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scantelburydev.cartracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
